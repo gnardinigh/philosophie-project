e9c2da85f2fff759fd43bf25c59d1bedf76e145c
gardiner nardini <gardiner.nardini@gmail.com>
2019-01-20 18:55:06 -0600

    header typo

--- src/header/RightText.jsx
+++ src/header/RightText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RightText = () =>
     <p className='header-right-text'>
-        Made by Gardiner Nardini for Steve Fuscaro @ Philosophie
+        Made by Gardiner Nardini for Steve Fusaro @ Philosophie
     </p>
 
 export default RightText;
