134291412d290ab02dbaef3c332c8e75658f04d9
gardiner nardini <gardiner.nardini@gmail.com>
2019-01-20 18:31:26 -0600

    fixed presentational components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
 import Home from './home';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, window.devToolsExtension ? window.devToolsExtension() : f => f)
+const store = createStore(
+    rootReducer, 
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+)
 
 ReactDOM.render(
     <Provider store={store}>
